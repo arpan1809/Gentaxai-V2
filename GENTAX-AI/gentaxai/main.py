d7ccfa388626a869f3427e39a30633211c151e69
arpan1809 <arpanchatterjee2004@gmail.com>
2025-11-16 20:02:11 +0530

    Update main.py

--- GENTAX-AI/gentaxai/main.py
+++ GENTAX-AI/gentaxai/main.py
@@ -119,3 +119,7 @@ if os.path.exists("static"):
 else:
     print("Warning: 'static' directory not found. The web interface will not be available.")
 
+if __name__ != "__main__":
+    vercel_app = app
+
+
